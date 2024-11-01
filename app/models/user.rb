1c139510ee22bd506060e1a7050c4e86464f921d
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-11-01 09:36:19 +0700

    Add model comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
